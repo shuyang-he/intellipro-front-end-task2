96cd2d5673cb38b41880e5128aba597db8192e8b
DESKTOP-0KBD6BC\Maxuncle2 <she26@wustl.edu>
2020-06-30 18:11:10 -0700

    add thunk in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,14 @@
 import reducer from "../reducers/index";
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
+const midwares = [thunk];
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(...midwares),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
